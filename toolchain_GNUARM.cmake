e1511f0e57e3a9651030cacdfabf43a769c01ae5
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-09-30 13:35:17 +0100

    [zep noup] build: gcc: Force DWARF v4
    This forces DWARF version 4 output so that zephyr debugging and usage
    still works with the pyelftools library which does not currently
    support v5.
    
    see https://github.com/zephyrproject-rtos/zephyr/issues/50373
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
    (cherry picked from commit 4a0b1a8e99d618025c5f9ece9b657a6cbb4bf83b)

--- toolchain_GNUARM.cmake
+++ toolchain_GNUARM.cmake
@@ -71,6 +71,8 @@ add_compile_options(
     $<$<COMPILE_LANGUAGE:C,CXX>:-g>
     $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:${TFM_DEBUG_OPTIMISATION}>,$<CONFIG:Debug>>:-Og>
     $<$<AND:$<COMPILE_LANGUAGE:C>,$<BOOL:${CONFIG_TFM_WARNINGS_ARE_ERRORS}>>:-Werror>
+    # Force DWARF version 4 for zephyr as pyelftools does not support version 5 at present
+    -gdwarf-4
 )
 
 # Pointer Authentication Code and Branch Target Identification (PACBTI) Options
